57e6772dc2088ce9a064d85f2a7abf7f5e4b2867
a.silnichenko <a.silnichenko@auamir.ru>
2018-09-26 08:50:54 +0700

    Добавление иконки

--- yii2-basic/views/layouts/main.php
+++ yii2-basic/views/layouts/main.php
@@ -17,6 +17,7 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
